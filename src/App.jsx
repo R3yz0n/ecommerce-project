86350a0b60ba22a5db465a03a911645395fb4a3c
r3yz0n <reyzon.thapa2020@gmail.com>
2025-05-18 12:45:35 +0545

    add:header,body and footer components

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 import React from "react";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import Footer from "./components/Footer";
 
 const App = () => {
-  return <div className="text-4xl">App</div>;
+  return (
+    <div className="text-4xl">
+      <Header />
+      <Body />
+      <Footer />
+    </div>
+  );
 };
 
 export default App;
