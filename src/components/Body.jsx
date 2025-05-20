73b54f8f8248e4309ddbd5fd99f07a37643e92b3
r3yz0n <reyzon.thapa2020@gmail.com>
2025-05-20 13:16:23 +0545

    ui built & passed data as props to ProductCard

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -13,20 +13,30 @@ const Body = () => {
         Search
       </div>
       <div className="flex flex-wrap justify-center gap-4 product-items">
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
-        <ProductCard />
+        <ProductCard
+          title="Ipad Pro 2020"
+          price={190}
+          description="Your perfect pack for everyday use and walks in the forest. Stash your laptop (up to 15 inches) in the padded sleeve, your everyday"
+          imageUrl="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-mini-2024-review-lead-672a0d53e55b6.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=1200:*"
+        />
+        <ProductCard
+          title="Samsung Galaxy Tab S7"
+          price={100.2}
+          description="Samsung Galaxy Tab S7 is a powerful tablet with a stunning display and versatile features."
+          imageUrl="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-mini-2024-review-lead-672a0d53e55b6.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=1200:*"
+        />
+        <ProductCard
+          title="Samsung Galaxy Tab S7"
+          price={100.2}
+          description="Samsung Galaxy Tab S7 is a powerful tablet with a stunning display and versatile features."
+          imageUrl="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-mini-2024-review-lead-672a0d53e55b6.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=1200:*"
+        />
+        <ProductCard
+          title="Samsung Galaxy Tab S7"
+          price={100.2}
+          description="Samsung Galaxy Tab S7 is a powerful tablet with a stunning display and versatile features."
+          imageUrl="https://hips.hearstapps.com/hmg-prod/images/apple-ipad-mini-2024-review-lead-672a0d53e55b6.jpg?crop=0.6666666666666666xw:1xh;center,top&resize=1200:*"
+        />
       </div>
     </section>
   );
