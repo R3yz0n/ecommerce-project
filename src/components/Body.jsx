6e0631824ad66d58e709d7e993c02c54fd04ec2a
r3yz0n <reyzon.thapa2020@gmail.com>
2025-06-08 14:15:20 +0545

    chore: update package dependencies for improved stability

--- src/components/Body.jsx
+++ src/components/Body.jsx
@@ -1,9 +1,9 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ProductCard from "./ProductCard";
-
 import Category from "./Category";
 import { ProductItems } from "../utils/mockData";
-
+import axios from "axios";
+//
 const Body = () => {
   const [filteredItems, setFilteredItems] = useState(ProductItems); // the first argument is the state variable, the second argument is the function to update the state variable
 
@@ -13,6 +13,15 @@ const Body = () => {
     });
     setFilteredItems(filteredProductsByRating);
   };
+  useEffect(() => {
+    // We will call api here
+    async function fetchProducts() {
+      const response = await axios.get("https://fakestoreapi.com/products");
+      setFilteredItems(response.data);
+    }
+
+    fetchProducts();
+  }, []);
   return (
     <section className="flex flex-col gap-4 px-2 py-2 mb-10  text-lg">
       <aside className="flex items-center justify-between gap-4 p-4 md:px-6 lg:px-8  rounded-lg">
@@ -51,17 +60,17 @@ export default Body;
 // for this component i used the flowbite
 const SearchBar = () => {
   return (
-    <form class="max-w-lg  w-2/4 mx-auto ml-0">
+    <form className="max-w-lg  w-2/4 mx-auto ml-0">
       <label
-        for="default-search"
-        class="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
       >
         Search
       </label>
-      <div class="relative">
-        <div class="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
+      <div className="relative">
+        <div className="absolute inset-y-0 start-0 flex items-center ps-3 pointer-events-none">
           <svg
-            class="w-4 h-4 text-gray-500 dark:text-gray-400"
+            className="w-4 h-4 text-gray-500 dark:text-gray-400"
             aria-hidden="true"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
@@ -79,13 +88,13 @@ const SearchBar = () => {
         <input
           type="search"
           id="default-search"
-          class="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          className="block w-full p-4 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           placeholder="Search Mockups, Logos..."
           required
         />
         <button
           type="submit"
-          class="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="text-white absolute end-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
         >
           Search
         </button>
